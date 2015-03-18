66bd1dcb357c74b60c931ec7494fcf30539320ad
Mert <mertcetkay@gmail.com>
2015-03-18 04:27:44 +0200

    model

---
+++ TrainsSolution/src/main/java/tr/com/taughtworks/hw/model/Graph.java
@@ -0,0 +1,27 @@
+package tr.com.taughtworks.hw.model;
+
+import java.util.List;
+
+/**
+ * Created by Mert on 18.3.2015.
+ */
+public class Graph {
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+
+
+}
