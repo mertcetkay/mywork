34504ed04e043eec0cd99a7c55629919a2a51b48
Mert <mertcetkay@gmail.com>
2015-03-21 18:29:40 +0200

    model

---
+++ TrainsSolution/src/main/java/tr/com/taughtworks/hw/algorithm/GraphTraverseAlgorithms.java
@@ -0,0 +1,39 @@
+package tr.com.taughtworks.hw.algorithm;
+
+import tr.com.taughtworks.hw.model.Vertex;
+
+import java.util.List;
+
+/**
+ * Created by Mert on 21.3.2015.
+ */
+public class GraphTraverseAlgorithms {
+
+    /**
+     * Constructor
+     */
+    public GraphTraverseAlgorithms(){
+
+    }
+
+    /**
+     * Calculates the distance of given route.
+     */
+    public int getTotalDistanceOfRoute(List<Vertex> route){
+        //Defensive programming
+        if( route.size() < 2){ return 0; }
+
+        int distance = 0;
+
+        return 0;
+    }
+
+    /**
+     * Calculates number of routes from start to end
+     * including maxStops vertice at most.
+     */
+    public int getNunberOfTrips(Vertex start, Vertex end, int maxStops){
+
+        return 0;
+    }
+}
